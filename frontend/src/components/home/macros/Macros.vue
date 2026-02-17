3e5fe846d7ca6915837c97ca06ccc7711e1661c8
timurenk0 <zheltenkovtimur@gmail.com>
2026-02-17 12:45:51 +0100

    Made calorie chart interactive. Users can switch between 'consumed' and 'remaining' calories views

--- frontend/src/components/home/macros/Macros.vue
+++ frontend/src/components/home/macros/Macros.vue
@@ -3,6 +3,9 @@ import DoughnutChart from './doughnut/DoughnutChart.vue';
 import BarChart from './bar/BarChart.vue';
 
 
+const totalCalories = 2200;
+const consumed = 1543;
+
 const macros = {
     Protein: {color: "#DB7070", consumed: 100, total: 150},
     Fat: {color: "#6296E0", consumed: 30, total: 65},
@@ -12,8 +15,8 @@ const macros = {
 
 <template>
     <section class="bg-white w-full rounded-lg">
-        <DoughnutChart  />
-        <div class="w-full flex gap-2 justify-between px-2 pb-4">
+        <DoughnutChart :totalCalories="totalCalories" :consumed="consumed" />
+        <div class="w-full flex gap-6 justify-between px-2 pb-4">
             <BarChart v-for="[k, v] in Object.entries(macros)"
                 :key="k"
                 :title="k"
--- frontend/src/components/home/macros/doughnut/DoughnutChart.vue
+++ frontend/src/components/home/macros/doughnut/DoughnutChart.vue
@@ -6,12 +6,23 @@ import {
     Tooltip,
     ArcElement
 } from "chart.js"
+import { ref } from 'vue';
 
 ChartJS.register(Title, Tooltip, ArcElement)
 
-const totalCalories = 2200;
-const consumed = 623;
+
+let chartView = ref("consumed");
+
+const toggleChartView = () => {
+    chartView.value === "consumed" ? chartView.value = "left" : chartView.value = "consumed";
+}
+
+const { totalCalories, consumed } = defineProps<{
+    totalCalories: number,
+    consumed: number;
+}>();
 const data = [consumed, totalCalories-consumed]
+
 
 const chartData = {
     labels: ["Consumed", "Remaining"],
@@ -31,11 +42,11 @@ const chartOptions = {
 </script>
 
 <template>
-    <div class="relative flex justify-center h-[200px]">
+    <div class="relative flex justify-center h-[200px]" v-on:click="toggleChartView">
         <Doughnut :options="chartOptions" :data="chartData" />
         <div class="absolute pt-6 inset-0 flex flex-col items-center justify-center">
-            <p class="text-text text-4xl font-semibold">{{ totalCalories-consumed }}</p>
-            <p class="text-ghost-text">Remaining</p>
+            <p class="text-text text-4xl font-semibold">{{ chartView === "consumed" ? consumed : totalCalories-consumed}}<span class="text-xs">/{{ totalCalories }}</span></p>
+            <p class="text-ghost-text">{{ chartView === "consumed" ? "Consumed" : "Remaining" }}</p>
         </div>
         <!-- <p class="absolute left-1 top-57">0</p>
         <p class="absolute right-1 top-57">{{ totalCalories }}</p> -->
