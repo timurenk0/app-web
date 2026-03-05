61a863f795618383f2dd0bfcaa885e03ab62ed20
timurenk0 <zheltenkovtimur@gmail.com>
2026-03-05 12:59:49 +0100

    Redesigned footer | Added animations on click

--- frontend/src/components/layout/Footer.vue
+++ frontend/src/components/layout/Footer.vue
@@ -1,27 +1,59 @@
 <script setup lang="ts">
 import { BookTextIcon, HomeIcon, Settings2Icon, TrendingUpIcon, UserIcon } from 'lucide-vue-next';
+import { ref } from 'vue';
 
+const activeIdx = ref(2);
 
+const items = [
+    { icon: BookTextIcon, href: "/recipies" },
+    { icon: TrendingUpIcon, href: "/stats" },
+    { icon: HomeIcon, href: "/" },
+    { icon: UserIcon, href: "/user" },
+    { icon: Settings2Icon, href: "/settings" },
+]
+
+function setActive(idx: number) {
+    activeIdx.value = idx;
+}
 </script>
 
 <template>
-    <footer class="fixed left-0 bottom-0 bg-white/60 w-full flex justify-between items-center px-5">
-        <a href="/recipies">
-            <BookTextIcon :size="32" />
-        </a>
-        <a href="/stats">
-            <TrendingUpIcon :size="32" />
-        </a>
-        <a href="/" class="relative bottom-10 bg-green border rounded-full px-4 py-4">
-            <HomeIcon :size="48" />
-        </a>
-        <a href="/user">
-            <UserIcon :size="32" />
-        </a>
-        <a href="/settings">
-            <Settings2Icon :size="32" />
-        </a>
+    <footer class="fixed left-0 bottom-0 w-full">
+        <div class="bg-white footer-shadow grid grid-cols-5 mx-4 my-2 py-4 rounded-full">
+            <div
+                v-for="(item, index) in items"
+                :key="index"
+                class="flex relative justify-center items-center" 
+            >
+            <a
+                :href="item.href"
+                @click.prevent="setActive(index)"
+                :class="{ active: activeIdx === index }"
+            >
+                <component :is="item.icon" :size="32" />
+            </a>
+            </div>
+        </div>
     </footer>
 </template>
 
-<style scoped></style>
+<style scoped>
+a {
+    display: inline-flex;
+    transition: transform 0.3s ease, background-color 0.3s ease;
+}
+
+.active {
+    position: absolute;
+    transform: translateY(calc(var(--spacing)*3));
+    background-color: var(--color-primary);
+    border-radius: 50%;
+    padding: calc(var(--spacing)*3);
+    margin-bottom: calc(var(--spacing)*6);
+    z-index: 10;
+}
+
+.footer-shadow {
+    box-shadow: 0 0 8px 0 var(--color-ghost-text);
+}
+</style>
