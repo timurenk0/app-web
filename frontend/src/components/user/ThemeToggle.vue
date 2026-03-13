2b37a1468e424f90197739d10020656ca4491306
timurenk0 <zheltenkovtimur@gmail.com>
2026-03-13 20:08:36 +0100

    Refactored theme toggle switch

--- frontend/src/components/user/ThemeToggle.vue
+++ frontend/src/components/user/ThemeToggle.vue
@@ -1,21 +1,17 @@
 <script setup lang="ts">
 import { useDarkMode } from "@l/useDarkMode";
 import { Moon, Sun } from "lucide-vue-next";
+import ToggleSwitch from "primevue/toggleswitch";
 
 const { isDark, toggle } = useDarkMode();
+
 </script>
 
 <template>
-    <button 
-        type="button" 
-        @click="toggle" 
-        class="p-2 rounded-full
-        text-gray-700 hover:text-gray-900
-        dark:text-gray-300 dark:hover:text-gray-100
-        hover:bg-gray-200 dark:hover:bg-gray-700
-        transition-colors focus:outline-none focus:ring-2 focus:ring-accent/50"
-    >
-        <Sun v-if="isDark" />
-        <Moon v-else />
-    </button>
+    <ToggleSwitch @change="toggle" v-model="isDark">
+        <template #handle="{ checked: isDark }">
+            <Sun v-if="!isDark" />
+            <Moon v-else />
+        </template>
+    </ToggleSwitch>
 </template>
