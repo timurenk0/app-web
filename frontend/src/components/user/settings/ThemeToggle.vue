006b396f971a62992db0cab3880a937a784dd2d3
timurenk0 <zheltenkovtimur@gmail.com>
2026-03-15 18:56:03 +0100

    Added Card around the theme toggle component

--- frontend/src/components/user/settings/ThemeToggle.vue
+++ frontend/src/components/user/settings/ThemeToggle.vue
@@ -2,16 +2,24 @@
 import { useDarkMode } from "@l/useDarkMode";
 import { Moon, Sun } from "lucide-vue-next";
 import ToggleSwitch from "primevue/toggleswitch";
+import Card from "primevue/card";
 
 const { isDark, toggle } = useDarkMode();
 
 </script>
 
 <template>
-    <ToggleSwitch @change="toggle" v-model="isDark">
-        <template #handle="{ checked: isDark }">
-            <Sun v-if="!isDark" />
-            <Moon v-else />
+    <Card class="w-full">
+        <template #content>
+            <div class="flex justify-between">
+                <p>Switch to {{ isDark ? 'light' : 'dark' }} mode</p>
+                <ToggleSwitch @change="toggle" v-model="isDark">
+                    <template #handle="{ checked: isDark }">
+                        <Sun v-if="!isDark" />
+                        <Moon v-else />
+                    </template>
+                </ToggleSwitch>
+            </div>
         </template>
-    </ToggleSwitch>
+    </Card>
 </template>
